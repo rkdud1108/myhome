b66827b8520e9298003f6d154fa71a637b8fe828
GY <rkdud1108@naver.com>
2022-01-22 21:44:04 +0900

    form 기본 기능

--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -1,11 +1,13 @@
 package com.godcoder.myhome.model;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +15,8 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
     private String content;
 }
