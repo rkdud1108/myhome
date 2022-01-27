e9124c73faf15868a9f6b7c7e1bf38be1f2e9bb8
GY <rkdud1108@naver.com>
2022-01-27 20:53:55 +0900

    Spring Security 적용 및 로그인 기능 구현

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -27,11 +27,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers("/", "/account/register", "/css/**", "/api/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/account/login")
                     .permitAll()
                     .and()
                 .logout()
@@ -46,10 +46,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery("select username, password, enabled "
                         + "from user "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select username, name "
+                .authoritiesByUsernameQuery("select u.username, r.name "
                         + "from user_role ur inner join user u on ur.user_id = u.id "
-                        + "inner join role r on ur.user_id = r.id "
-                        + "where email = ?");
+                        + "inner join role r on ur.role_id = r.id "
+                        + "where u.username = ?");
     }
     //Authentication 로그인
     //Authorization 권한
